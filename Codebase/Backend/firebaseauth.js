0301323b86420d459852d59193ab0d23096e4886
Landon Clarke <89565789+Larke20@users.noreply.github.com>
2024-10-16 14:53:36 -0500

    fixed api key path (was missing file ext...)

--- Codebase/Backend/firebaseauth.js
+++ Codebase/Backend/firebaseauth.js
@@ -1,24 +1,29 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-  import { firebaseConfig } from "./apiKey";
+  import { firebaseConfig } from "./apiKey.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-
+  
   //input fields
   document.addEventListener("DOMContentLoaded", function() {
     const email = document.getElementById('create-email').value;
     const username = document.getElementById('create-username').value;
     const password = document.getElementById('create-password').value;
-});
+  });
 
-  //login/create acct button function
+  //create acct button function
   const createSubmit = document.getElementById('create-submit');
   createSubmit.addEventListener('click', function(event){
     event.preventDefault();
     alert(5);
   })
+  //login button function
   
+  const loginSubmit = document.getElementById('login-submit');
+  loginSubmit.addEventListener('click', function(event){
+    event.preventDefault();
+    alert(10);
+  })
