9ac05124ec1787e85d1f8e89f2384243aac2832f
Connor Morris <cmorr78@lsu.edu>
2024-10-22 11:29:03 -0500

    Partially implement calendar using fullcalendar.js

---
+++ Codebase/Frontend/CalendarUI/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      events: [] /* events go into this array */
+    });
+    
+    calendar.render();
+});
