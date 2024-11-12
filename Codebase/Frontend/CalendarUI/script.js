e2e22727de380d3a3d2357691de5070b1c901ed4
Connor Morris <cmorr78@lsu.edu>
2024-11-12 11:15:55 -0600

    Added Firestore-related code for the Calendar

--- Codebase/Frontend/CalendarUI/script.js
+++ Codebase/Frontend/CalendarUI/script.js
@@ -1,87 +1,73 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      editable: true,
-      clickable: true,
-      dayMaxEvents: true,
-      navLinks: true,
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      footerToolbar: {
-        center: 'addTaskButton',
-      },
-      customButtons: {
-        addTaskButton: {
-          text: 'Add a new task...',
-          click: function() {
-            var dateStr = prompt('Enter a date in the format of YYYY-MM-DD');
-            var date = new Date(dateStr + 'T00:00:00') // using JS Date object
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
 
-            if (!isNaN(date.valueOf())) { // check for valid date before adding
-              calendar.addEvent({ // addEvent() works locally, it doesn't add to the DB
-                title: 'Demo Task (Dynamic)',
-                start: date,
-                allDay: true
-              });
-              alert('Task added!');
-            } else {
-              alert('Invalid date.');
-            }
+const firebaseConfig = {
+  apiKey: "AIzaSyAAxXs88pJUfCoeotb0C8gfTGxvltpPBz8",
+  authDomain: "clarity-295d8.firebaseapp.com",
+  databaseURL: "https://clarity-295d8-default-rtdb.firebaseio.com",
+  projectId: "clarity-295d8",
+  storageBucket: "clarity-295d8.firebasestorage.app",
+  messagingSenderId: "117186684063",
+  appId: "1:117186684063:web:a0a70113604e5c07ed2eaa"
+};
+
+// Initialize Firebase & Cloud Firestore; get a reference to Firestore
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Main function to work with FullCalendar
+function fullCalendarMain() {
+  // Get event data from Firestore (example code from Google)
+  /*
+  db.collection("users").get().then((querySnapshot) => { 
+    querySnapshot.forEach((doc) => { 
+      console.log(`${doc.id} => ${doc.data()}`); 
+    });
+  });
+  */
+  
+  // Find HTML calendar element
+  var calendarEl = document.getElementById('calendar');
+  
+  // Instance new calendar object
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    editable: true,
+    clickable: true,
+    dayMaxEvents: true,
+    navLinks: true,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    footerToolbar: {
+      center: 'addTaskButton',
+    },
+    customButtons: {
+      addTaskButton: {
+        text: 'Add a new task...',
+        click: function() {
+          var dateStr = prompt('Enter a date in the format of YYYY-MM-DD');
+          var date = new Date(dateStr + 'T00:00:00') // Using JS Date object
+
+          if (!isNaN(date.valueOf())) { // Check for valid date before adding
+            calendar.addEvent({ // addEvent() works locally, it doesn't add to the DB (TODO: fix this)
+              title: 'Demo Task (Dynamic)',
+              start: date,
+              allDay: true
+            });
+            alert('Task added!');
+          } else {
+            alert('Invalid date.');
           }
         }
-      },
-      events: [ // set of demo events to show off functionality
-        {
-          title: 'All Day Event',
-          start: '2024-10-01',
-        },
-        {
-          title: 'Long Event',
-          start: '2024-10-07',
-          end: '2024-10-10'
-        },
-        {
-          title: 'Meeting 1',
-          start: '2024-10-21T10:30:00',
-          end: '2024-10-21T12:30:00'
-        },
-        {
-          title: 'Meeting on Zoom',
-          url: 'https://zoom.us/',
-          start: '2024-10-23T14:30:00',
-          end: '2024-10-23T16:30:00'
-        },
-        {
-          title: 'Task Due',
-          start: '2024-10-26T12:00:00',
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2024-10-04T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2024-10-11T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2024-10-18T16:00:00'
-        },
-        {
-          groupId: 999,
-          title: 'Repeating Event',
-          start: '2024-10-25T16:00:00'
-        }
-      ]
-    });
-    
-    calendar.render();
-});
+      }
+    }
+  });
+  
+  calendar.render(); // Display the calendar on the page
+}
+
+// Event listener for activating the FullCalendar library upon page load
+document.addEventListener('DOMContentLoaded', fullCalendarMain());
