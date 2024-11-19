d728af624d42ed85c0c2fa0a721b47aad2fa8b9c
AlleeR <94555083+AlleeR@users.noreply.github.com>
2024-11-19 15:41:45 -0600

    Add check for if new passwords entered match.

--- Codebase/Frontend/UserProfile/script.js
+++ Codebase/Frontend/UserProfile/script.js
@@ -10,6 +10,9 @@ const saveBtn = document.querySelector('.save');
 const closeBtn = document.querySelector('.close');
 const updatePasswordBtn = document.querySelector('.passwdbtn');
 
+const closeMissmatchBtn = document.querySelector('#close_missmatch_popup');
+const closeSucessfulBtn = document.querySelector('#close_sucessful_popup');
+
 const newPassword = document.getElementById('password1');
 const newPasswordCheck = document.getElementById('password2');
 
@@ -26,15 +29,38 @@ saveBtn.addEventListener('click', function(){
 
     if (newPassword.value == newPasswordCheck.value ){
 
+        /*
         updatePassword(user, newPassword).then(() => {
             // Update successful.
         }).catch((error) => {
             // An error ocurred
             // ...
         });
+        */
+
+        //show pupup for a sucessfully updated password
+        document.getElementById("sucessful").style.display = "block";
+
+        
+
+    }
+    else{
+        //show pupup for a sucessfully updated password
+        document.getElementById("missmatched").style.display = "block";
+    
+        
 
     }
 
+});
+
+closeSucessfulBtn.addEventListener('click', function(){
+    document.getElementById("sucessful").style.display = "none";
+    document.getElementById("password_overlay").style.display = "none";
+});
+
+closeMissmatchBtn.addEventListener('click', function(){
+    document.getElementById("missmatched").style.display = "none";
 });
 
 //show overlay if change password button is selected
