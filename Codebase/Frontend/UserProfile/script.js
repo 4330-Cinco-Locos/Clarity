6ed7d3540a2757950426d26d5e1faf93507553b7
AlleeR <94555083+AlleeR@users.noreply.github.com>
2024-11-19 15:07:42 -0600

    Fix issue with page linking and popup not opening in the user profile

--- Codebase/Frontend/UserProfile/script.js
+++ Codebase/Frontend/UserProfile/script.js
@@ -1,27 +1,48 @@
-import { onAuthStateChanged, auth } from "../../Backend/firebaseauth.js";
+console.log("script.js is running");
+
 
 const toggleBtn = document.querySelector('.toggle_button');
 const toggleBtnIco = document.querySelector('.toggle_button i');
 const dropdownM = document.querySelector('.dropdown');
 const signOutBtn = document.querySelector('.act_btn');
 
+const saveBtn = document.querySelector('.save');
+const closeBtn = document.querySelector('.close');
+const updatePasswordBtn = document.querySelector('.passwdbtn');
 
-// Track the user authentication state
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in
+const newPassword = document.getElementById('password1');
+const newPasswordCheck = document.getElementById('password2');
 
-        // Example: Log the current user UID to console
-        console.log("Curr User UID: ", user.uid);
 
-    } else {
-        // User is signed out
-        console.log("No user is signed in.");
-    }
-});
 // togglebtn is the bars dropdown icon
 toggleBtn.addEventListener('click', function() {
     dropdownM.classList.toggle('open');
     const isOpen = dropdownM.classList.contains('open');
     toggleBtnIco.className = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+});
+
+//update user's password when the 'save' button is selected
+saveBtn.addEventListener('click', function(){
+
+    if (newPassword.value == newPasswordCheck.value ){
+
+        updatePassword(user, newPassword).then(() => {
+            // Update successful.
+        }).catch((error) => {
+            // An error ocurred
+            // ...
+        });
+
+    }
+
+});
+
+//show overlay if change password button is selected
+updatePasswordBtn.addEventListener('click', function(){
+    document.getElementById("password_overlay").style.display = "block";
+});
+
+//close the update password display
+closeBtn.addEventListener('click', function(){
+    document.getElementById("password_overlay").style.display = "none";
 });
