12cba8f9f2d3d407c74eb3eeddff5b7e5839e600
Connor Morris <cmorr78@lsu.edu>
2024-11-21 01:22:49 -0600

    Changed Calendar library, added dependency

--- Codebase/Frontend/CalendarUI/script.js
+++ Codebase/Frontend/CalendarUI/script.js
@@ -1,64 +1,30 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAAxXs88pJUfCoeotb0C8gfTGxvltpPBz8",
-  authDomain: "clarity-295d8.firebaseapp.com",
-  databaseURL: "https://clarity-295d8-default-rtdb.firebaseio.com",
-  projectId: "clarity-295d8",
-  storageBucket: "clarity-295d8.firebasestorage.app",
-  messagingSenderId: "117186684063",
-  appId: "1:117186684063:web:a0a70113604e5c07ed2eaa"
-};
+// Find HTML calendar element
+var calendarElement = document.getElementById('calendar');
 
-// Event listener for activating the FullCalendar library upon page load
-document.addEventListener('DOMContentLoaded', function() {
-  // Find HTML calendar element
-  var calendarEl = document.getElementById('calendar');
-  
-  // Instance new calendar object
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    editable: true,
-    dayMaxEvents: true,
-    navLinks: true,
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
-    },
-    footerToolbar: {
-      center: 'addTaskButton',
-    },
-    customButtons: {
-      addTaskButton: {
-        text: 'Add a new task...',
-        click: function() {
-          var dateStr = prompt('Enter a date in the format of YYYY-MM-DD');
-          var date = new Date(dateStr + 'T00:00:00') // Using JS Date object
-
-          if (!isNaN(date.valueOf())) { // Check for valid date before adding
-            calendar.addEvent({ // addEvent() works locally, it doesn't add to the DB (TODO: fix this)
-              title: 'Demo Task (Dynamic)',
-              start: date,
-              allDay: true
-            });
-            alert('Task added!');
-          } else {
-            alert('Invalid date.');
-          }
-        }
-      }
-    },
-    eventSources: [
-      {
-        id: 1,
-        url: 'https://clarity-295d8-default-rtdb.firebaseio.com/eventDB.json',
-        method: 'GET',
-        format: 'json',
-        failure: function() {
-          alert('There was an error while fetching events!');
-        }
-      }
-    ]
-  });
-  
-  calendar.render(); // Display the calendar on the page
+// Create new instance of a Calendar object
+var calendarInstance = new calendarJs(calendarElement, {
+  exportEventsEnabled: false,
+  manualEditingEnabled: false,
+  useAmPmForTimeDisplays: true
 });
+
+// Asynchronous function to import events into the Calendar from Firebase
+async function getEvents() {
+  const url = 'https://clarity-295d8-default-rtdb.firebaseio.com/eventDB_2.json';
+  try {
+    const response = await fetch(url); // Make HTTP request to URL, await the response
+    if (!response.ok) { // If the response status isn't OK (200), throw an error containing the actual status
+      throw new Error('HTTP Status ' + response.status + ', ' + response.statusText);
+    }
+    const data = await response.json(); // Upon fetch success, parse the response data as a JSON object
+
+    console.log(data); // Debug
+    console.log(JSON.stringify(data)); // Debug
+    
+    calendarInstance.addEventsFromJson(data); // Set calendar events using the newly acquired JSON
+  } catch (error) { // Send error message to the browser console
+    console.error(error.message);
+  }
+}
+
+getEvents();
