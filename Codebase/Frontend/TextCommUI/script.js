39a2d0f49de0510412b639912b9dc7bda8824059
LaneDurst <143672256+LaneDurst@users.noreply.github.com>
2024-11-25 17:26:03 -0600

    removed the file upload button, added dynamic tracking of the currentUser

--- Codebase/Frontend/TextCommUI/script.js
+++ Codebase/Frontend/TextCommUI/script.js
@@ -1,15 +1,9 @@
 import {getDatabase, set, get, update, remove, ref, child, onValue} from  "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js"
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import { auth } from "../../Backend/firebaseauth.js" 
 
-const firebaseConfig = {
-apiKey: "AIzaSyAAxXs88pJUfCoeotb0C8gfTGxvltpPBz8",
-authDomain: "clarity-295d8.firebaseapp.com",
-databaseURL: "https://clarity-295d8-default-rtdb.firebaseio.com",
-projectId: "clarity-295d8",
-storageBucket: "clarity-295d8.firebasestorage.app",
-messagingSenderId: "117186684063",
-appId: "1:117186684063:web:a0a70113604e5c07ed2eaa"
-};
+import { firebaseConfig } from "../../Backend/apiKey.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -17,41 +11,58 @@ const app = initializeApp(firebaseConfig);
 // initializing variables
 const db = getDatabase();
 const dbRef = ref(db, 'chat/');
-const textField = document.getElementById("myText")  // whatever text the user has entered in the textbox
-var userId = "Lane" // TODO: set dynamically
-var imgUrl = "" // TODO: set dynamically
+const textField = document.getElementById("myText");  // whatever text the user has entered in the textbox
+var userId = "loggedOut"; // TODO: set dynamically
+var imgUrl = ""; // TODO: set dynamically
+
+// this is the setter method for the userId var
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      userId = user.uid;
+    }
+    else
+    {
+        alert("Only Logged in users may view and send messages\n")
+    }
+  });
+  
+
 
 // This will read all entries from firebase and add them to the message container
 onValue(dbRef,(snapshot) =>{
-
-    snapshot.forEach(function(childSnapshot)
+    if(userId != "loggedOut")
     {
-        const childData = childSnapshot.val();
-        const storedId = childData.senderId
+        snapshot.forEach(function(childSnapshot)
+        {
+            const childData = childSnapshot.val();
+            const storedId = childData.senderId
 
-        //making the main message container
-        const newMessage = document.createElement("div");
-        newMessage.classList.add("message");
+            //making the main message container
+            const newMessage = document.createElement("div");
+            newMessage.classList.add("message");
 
-        //making the message sub-elements
-        const senderElement = document.createElement("div");
-        senderElement.classList.add("sender");
-        senderElement.textContent = childData.senderId;
+            //making the message sub-elements
+            const senderElement = document.createElement("div");
+            senderElement.classList.add("sender");
+            senderElement.textContent = childData.senderId;
 
-        const bodyElement = document.createElement("div");
-        bodyElement.classList.add("body");
-        bodyElement.textContent = childData.body;
+            const bodyElement = document.createElement("div");
+            bodyElement.classList.add("body");
+            bodyElement.textContent = childData.body;
 
-        const timeElement = document.createElement("div");
-        timeElement.classList.add("time");
-        timeElement.textContent = childData.timeStamp.toString();
+            const timeElement = document.createElement("div");
+            timeElement.classList.add("time");
+            timeElement.textContent = childData.timeStamp.toString();
 
-        newMessage.appendChild(senderElement);
-        newMessage.appendChild(bodyElement);
-        newMessage.appendChild(timeElement);
+            newMessage.appendChild(senderElement);
+            newMessage.appendChild(bodyElement);
+            newMessage.appendChild(timeElement);
 
-        document.getElementById("content").appendChild(newMessage);
-    });
+            document.getElementById("content").appendChild(newMessage);
+        });
+    }
+    else{console.log("Users who are not logged in may not view messages!\n");}
 }, (errorObject) => {
     console.log('The read failed: '+ errorObject.name)
 });
@@ -62,20 +73,25 @@ onValue(dbRef,(snapshot) =>{
 //CRUD methods
 function sendData() // Create a new entry on the database
 {
-var time = new Date().toUTCString();
-var msgId = userId+time;
-set(ref(db, "chat/"+msgId), {
-    senderId: userId,
-    body: textField.value, //have to grab the values IN the element, not the element itself
-    imageUrl: imgUrl, //this will also need a .value once it is properly implemented
-    timeStamp: time
-})
-.then(()=>{
-    alert("Message sent")
-})
-.catch((error)=>{
-    alert(error)
-})
+    if(userId != "loggedOut")
+    {
+        var time = new Date().toUTCString();
+        var msgId = userId+time;
+        set(ref(db, "chat/"+msgId), {
+            senderId: userId,
+            body: textField.value, //have to grab the values IN the element, not the element itself
+            imageUrl: imgUrl, //this will also need a .value once it is properly implemented
+            timeStamp: time
+        })
+        .catch((error)=>{
+            alert(error)
+        })
+    }
+    else
+    {
+        alert("Users who are not logged in may not send messages!\n");
+        console.log("Users who are not logged in may not send messages!\n");
+    }
 }
 
 // this might need to be deleted later as it doesn't really have much of a use? //
@@ -126,8 +142,9 @@ function handleFileSelect(event) {
 textField.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         sendData();
+        textField.value = "";
     }
 });
 
-fileBtn.addEventListener('click', function(){fileInput.click()});
-fileInput.addEventListener('change', function(){handleFileSelect(event)});
+//fileBtn.addEventListener('click', function(){fileInput.click()});
+//fileInput.addEventListener('change', function(){handleFileSelect(event)});
