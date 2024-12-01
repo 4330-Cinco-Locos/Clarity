19446c175a75dfdbd4275e5aed57fd9184e77437
Landon Clarke <89565789+Larke20@users.noreply.github.com>
2024-12-01 13:23:51 -0600

    took apikey out of gitignore

---
+++ Codebase/Backend/apiKey.js
@@ -0,0 +1,10 @@
+//firebase api deets
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAxXs88pJUfCoeotb0C8gfTGxvltpPBz8",
+    authDomain: "clarity-295d8.firebaseapp.com",
+    projectId: "clarity-295d8",
+    storageBucket: "clarity-295d8.appspot.com",
+    messagingSenderId: "117186684063",
+    appId: "1:117186684063:web:a0a70113604e5c07ed2eaa"
+  };
